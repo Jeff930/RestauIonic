15670427c2668dbf030548e5db3fb090cf651916
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-07 15:01:25 +0800

    Added outside models

---
+++ src/models/outside.ts
@@ -0,0 +1,31 @@
+export const outside = [
+	{
+		name : 'sample Outside 1',
+		image: 'outside/outside-1.jpg'
+		
+	}, {
+		name: 'sample Outside 2',
+		image: 'outside/outside-2.jpg'
+		
+		
+	}, {
+		name: 'sample Outside 3',
+		image: 'outside/outside-3.jpg'
+		
+
+	}, {
+		name: 'sample Outside 4',
+		image: 'outside/outside-4.jpg'
+		
+
+	}, {
+		name: 'sample Outside 5',
+		image: 'outside/outside-5.jpg'
+		
+
+	}, {
+		name: 'sample Outside 6',
+		image: 'outside/outside-6.jpg'
+		
+	}
+];
