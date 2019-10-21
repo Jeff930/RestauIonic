c6549b036a3e4365d7d341ce580fea58b089bc60
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-21 14:16:38 +0800

    Added chicken model

---
+++ src/models/chicken.ts
@@ -0,0 +1,15 @@
+export const chickens = [
+	{
+		name : 'Chicken Curry',
+		description : 'Chicken stewed in coconut milk and curry spices.',
+		image: 'chicken/ChickenCurry.jpg',
+		price : '12.99'
+		
+	},{
+		name : 'Chicken BBQ (4 skewers/1 skewer)',
+		description : 'Grilled Chicken BBQ skewers served with mixed vinegar and soy sauce.',
+		image: 'chicken/ChickenBBQ.jpg',
+		price : '10.99/2.9'
+		
+	}
+];
