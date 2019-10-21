47e47a6c22260e07002ecc0190e496aa52140428
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-21 12:45:33 +0800

    Added appetizer contents

--- src/models/appetizer.ts
+++ src/models/appetizer.ts
@@ -1,31 +1,9 @@
-export const outside = [
+export const appetizers = [
 	{
-		name : 'sample Outside 1',
-		image: 'outside/outside-1.jpg'
-		
-	}, {
-		name: 'sample Outside 2',
-		image: 'outside/outside-2.jpg'
-		
-		
-	}, {
-		name: 'sample Outside 3',
-		image: 'outside/outside-3.jpg'
-		
-
-	}, {
-		name: 'sample Outside 4',
-		image: 'outside/outside-4.jpg'
-		
-
-	}, {
-		name: 'sample Outside 5',
-		image: 'outside/outside-5.jpg'
-		
-
-	}, {
-		name: 'sample Outside 6',
-		image: 'outside/outside-6.jpg'
+		name : 'Egg Rolls',
+		description : 'Deep-fried egg rolls consists of ground pork and veggies',
+		image: 'appetizer/EggRolls.jpg',
+		price : '7.99'
 		
 	}
 ];
