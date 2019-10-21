de69f8cca78bdaf79e45ab3e36c5aac0ec4e3d97
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-21 12:49:57 +0800

    Added vegetable model

---
+++ src/models/vegetable.ts
@@ -0,0 +1,9 @@
+export const appetizers = [
+	{
+		name : 'Egg Rolls',
+		description : 'Deep-fried egg rolls consists of ground pork and veggies',
+		image: 'appetizer/EggRolls.jpg',
+		price : '7.99'
+		
+	}
+];
