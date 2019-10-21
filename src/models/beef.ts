bdfbf476ebc80b591b1006fb24f4f920e6ff542d
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-21 13:33:39 +0800

    Added beef model

---
+++ src/models/beef.ts
@@ -0,0 +1,21 @@
+export const beefs = [
+	{
+		name : 'Beef Mechado',
+		description : 'Filipino beef stew with potatoes, carrots and onions.',
+		image: 'beef/BeefMechado.jpg',
+		price : '13.99'
+		
+	},{
+		name : 'Pinakbet',
+		description : 'A vegetable stew prepared with fresh vegetables sauteed with either pork, shrimp, and shrimp paste.',
+		image: 'vegetable/Pinakbet.jpg',
+		price : '12.99'
+		
+	},{
+		name : 'Lumpiang Sariwa',
+		description : 'Fresh spring roll made out of different vegetables such as carrots and bamboo shoots.',
+		image: 'vegetable/FreshLumpia.jpg',
+		price : '8.99'
+		
+	}
+];
