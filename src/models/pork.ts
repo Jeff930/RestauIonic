ae2e352d2aaf81a2c02290478ce6dee68d3f0ac3
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-21 23:00:23 +0800

    Added pork contents

--- src/models/pork.ts
+++ src/models/pork.ts
@@ -1,15 +1,34 @@
-export const chickens = [
+export const porks = [
 	{
-		name : 'Chicken Curry',
-		description : 'Chicken stewed in coconut milk and curry spices.',
-		image: 'chicken/ChickenCurry.jpg',
+		name : 'Crispy Pata',
+		description : 'Premium pork hock simmered in a special marinade and deep-fried to perfection.',
+		image: 'pork/CrispyPata.jpg',
+		price : '19.99'
+		
+	},{
+		name : 'Dinakdakan',
+		description : 'Grilled pork seasoned with onions,pepper, vinegar and mayonnaise.',
+		image: 'pork/Dinakdakan.jpg',
 		price : '12.99'
 		
 	},{
-		name : 'Chicken BBQ (4 skewers/1 skewer)',
-		description : 'Grilled Chicken BBQ skewers served with mixed vinegar and soy sauce.',
-		image: 'chicken/ChickenBBQ.jpg',
-		price : '10.99/2.99'
+		name : 'Pork Adobo',
+		description : 'Marinated pork chunks slowly cooked in vinegar and soy sauce with garlic,bay leaf and peppercorn.',
+		image: 'pork/PorkAdobo.jpg',
+		price : '13.99'
+		
+	},{
+		name : 'Pork BBQ(4 skewers/ 1 skewer)',
+		description : 'Delicious marinated pork sliced, skewered until its perfectly done.',
+		image: 'pork/PorkBBQ.jpg',
+		price : '10.99/2.75'
+		
+	},{
+		name : 'Crispy Binagoongan',
+		description : 'Combination of filipino favorites.',
+		image: 'pork/Dinakdakan.jpg',
+		price : '12.99'
 		
 	}
+
 ];
