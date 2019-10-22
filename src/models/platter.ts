4e2d9bbf74885561a6b584a23d1c771f69be6e19
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-22 15:36:11 +0800

    Created platter model

---
+++ src/models/platter.ts
@@ -0,0 +1,9 @@
+export const seafoods = [
+	{
+		name : 'Fried Bangus',
+		description : 'Fried marinated milkfish in vinegar, garlic and pepper.',
+		image: 'seafood/FriedBangus.jpg',
+		price : '13.99'
+		
+	}
+];
