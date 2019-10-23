0d19a2f532c1aea4fa1d7182bbc5f85f0a4393f8
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-23 16:56:50 +0800

    Added new dishes to vegetables

--- src/models/vegetable.ts
+++ src/models/vegetable.ts
@@ -1,6 +1,12 @@
 export const vegetables = [
 	{
-		name : 'Ginataang Gulay',
+		name : 'Chopsuey',
+		description : 'Chinese-inspires assorted vegetables with shrimp, pork or chicken sauteed in a savory sauce.',
+		image: 'Vegetable/Chopsuey.jpeg',
+		price : '12.99'
+		
+	},{
+		name : 'Ginataang Gulay (vegetables in Coconut Milk)',
 		description : 'Fresh vegetables cooked in coconut milk. Can be ordered with shrimp, pork or chicken.',
 		image: 'Vegetable/GinataangGulay.jpeg',
 		price : '12.99'
@@ -10,6 +16,12 @@ export const vegetables = [
 		description : 'A vegetable stew prepared with fresh vegetables sauteed with either pork, shrimp, and shrimp paste.',
 		image: 'Vegetable/Pinakbet.jpeg',
 		price : '12.99'
+		
+	},{
+		name : 'Laing (Taro leaves with Coconut Milk)',
+		description : 'Spicy taro leaves simmered in coconut milk with shrimp and pork.',
+		image: 'Vegetable/Laing.jpeg',
+		price : '10.99'
 		
 	},{
 		name : 'Lumpiang Sariwa',
