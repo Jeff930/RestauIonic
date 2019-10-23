fdb10467043c6b7d291f6714fde62352166a9fb4
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-23 16:19:01 +0800

    Completed appetizer model

--- src/models/appetizer.ts
+++ src/models/appetizer.ts
@@ -1,9 +1,39 @@
 export const appetizers = [
 	{
-		name : 'Egg Rolls',
+		name : 'Lumpiang Shanghai (Egg Roll)',
 		description : 'Deep-fried egg rolls consists of ground pork and veggies',
 		image: 'appetizer/EggRolls.jpg',
 		price : '7.99'
+		
+	},{
+		name : 'Vegetable Egg Rolls',
+		description : 'Deep-fried vegetable(carrots, cabbage, green beans) egg rolls.',
+		image: 'appetizer/VegetableEggRolls.jpg',
+		price : '7.99'
+		
+	},{
+		name : "Tokwa't Baboy (Fried Pork and Tofu)",
+		description : 'Fried pork ruffle - Deep-fried tofu cubes and pork pieces with a dipping mixture of soy sauce, vinegar and chillies',
+		image: 'appetizer/TokwaBaboy.jpg',
+		price : '7.99'
+		
+	},{
+		name : 'Calamari',
+		description : 'Squid rings that are battered and cooked deep-fried. Served with house special aioli dip.',
+		image: 'appetizer/Calamari.jpg',
+		price : '8.99'
+		
+	},{
+		name : 'French Fries',
+		description : 'Deep-fried very thin, salted slices of potato.',
+		image: 'appetizer/FrenchFries.jpg',
+		price : '7.99'
+		
+	},{
+		name : 'Chicharon Bulaklak (Crispy Pork Ruffle)',
+		description : 'Golden crisp pork ruffle fat served with a vinegar garlic dip.',
+		image: 'appetizer/EggRolls.jpg',
+		price : '8.99'
 		
 	}
 ];
