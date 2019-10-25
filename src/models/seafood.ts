c5360fb2b07639406ad123e26e00e35d9e45aa0b
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-25 15:20:58 +0800

    Added new seafood dishes

--- src/models/seafood.ts
+++ src/models/seafood.ts
@@ -13,15 +13,27 @@ export const seafoods = [
 		
 	},{
 		name : 'Pinaputok na Bangus (Fried and Popped Milkfish)',
-		description : 'Marinated and grilled mussels.',
-		image: 'seafood/InihawnaTahong.jpg',
-		price : '12.99'
+		description : 'Fried/popped Milkfish belly with tomatoes, onions and garlic.',
+		image: 'seafood/PinaputoknaBangus.jpg',
+		price : '14.99'
 		
 	},{
-		name : 'Fried Bangus',
+		name : 'Pinaputok na Pampano (Fried and Popped Pampano)',
+		description : 'Fried/popped Pampano fish with tomatoes, onions and garlic.',
+		image: 'seafood/PinaputoknaPampano.jpg',
+		price : '14.99'
+		
+	},{
+		name : 'Fried Bangus (Fried Milkfish)',
 		description : 'Fried marinated milkfish in vinegar, garlic and pepper.',
 		image: 'seafood/FriedBangus.jpg',
 		price : '13.99'
+		
+	},{
+		name : 'Fried Tilapia',
+		description : 'Fried tilapia seasoned with salt and pepper.',
+		image: 'seafood/FriedTilapia.jpg',
+		price : '11.99'
 		
 	}
 ];
