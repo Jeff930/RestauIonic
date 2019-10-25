faf52e51de6e6f0c18749748027abc5961026889
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-25 14:45:28 +0800

    Added new dishes to seafood

--- src/models/seafood.ts
+++ src/models/seafood.ts
@@ -1,5 +1,23 @@
 export const seafoods = [
 	{
+		name : 'Inihaw na Tahong (Grilled Mussels)',
+		description : 'Marinated and grilled mussels.',
+		image: 'seafood/InihawnaTahong.jpg',
+		price : '12.99'
+		
+	},{
+		name : 'Inihaw na Pusit (Grilled Squid)',
+		description : 'Marinated and grilled mussels.',
+		image: 'seafood/InihawnaTahong.jpg',
+		price : '12.99'
+		
+	},{
+		name : 'Pinaputok na Bangus (Fried and Popped Milkfish)',
+		description : 'Marinated and grilled mussels.',
+		image: 'seafood/InihawnaTahong.jpg',
+		price : '12.99'
+		
+	},{
 		name : 'Fried Bangus',
 		description : 'Fried marinated milkfish in vinegar, garlic and pepper.',
 		image: 'seafood/FriedBangus.jpg',
