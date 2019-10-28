5f12b05d92e188dd616ddd0df3f1bb26fafe6bf5
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-28 16:38:21 +0800

    Finished soup model

--- src/models/soup.ts
+++ src/models/soup.ts
@@ -1,81 +1,45 @@
-export const seafoods = [
+export const soups = [
 	{
-		name : 'Inihaw na Tahong (Grilled Mussels)',
-		description : 'Marinated and grilled mussels.',
-		image: 'seafood/InihawnaTahong.jpg',
+		name : 'Bulalo (Beef Marrow Stew)',
+		description : 'Tender beef shanks and ribs in a flavorful soup with green onions, peppercorn and potatoes.',
+		image: 'soup/Bulalo.jpg',
+		price : '15.99'
+		
+	},{
+		name : 'Beef Sinigang (Tamarind Soup)',
+		description : 'Beef stewed in a flavorful, slightly sour tamarind broth with lots of vegetables.',
+		image: 'soup/BeefSinigang.jpg',
+		price : '14.99'
+		
+	},{
+		name : 'Pork Sinigang (Pork Tamarind Soup)',
+		description : 'Pork stewed in a flavorful, slightly sour tamarind broth with lots of vegetables.',
+		image: 'soup/PorkSinigang.jpg',
 		price : '12.99'
 		
 	},{
-		name : 'Inihaw na Pusit (Grilled Squid)',
-		description : 'Marinated and grilled mussels.',
-		image: 'seafood/InihawnaTahong.jpg',
+		name : 'Shrimp Sinigang (Shrimp Tamarind Soup)',
+		description : 'Shrimp stewed in a flavorful, slightly sour tamarind broth with lots of vegetables.',
+		image: 'soup/ShrimpSinigang.jpg',
 		price : '12.99'
 		
 	},{
-		name : 'Pinaputok na Bangus (Fried and Popped Milkfish)',
-		description : 'Fried/popped Milkfish belly with tomatoes, onions and garlic.',
-		image: 'seafood/PinaputoknaBangus.jpg',
+		name : 'Bangus Sinigang (Milkfish Tamarind Soup)',
+		description : 'Milkfish stewed in a flavorful, slightly sour tamarind broth with lots of vegetables.',
+		image: 'soup/MilkfishSinigang.jpg',
 		price : '14.99'
 		
 	},{
-		name : 'Pinaputok na Pampano (Fried and Popped Pampano)',
-		description : 'Fried/popped Pampano fish with tomatoes, onions and garlic.',
-		image: 'seafood/PinaputoknaPampano.jpg',
-		price : '14.99'
+		name : 'Tinolang Manok (Chicken in Ginger Soup)',
+		description : 'Chicken soup with pepper leaves in broth flavored with ginger, onions and fish sauce.',
+		image: 'soup/TinolangManok.jpg',
+		price : '12.99'
 		
 	},{
-		name : 'Fried Bangus (Fried Milkfish)',
-		description : 'Fried marinated milkfish in vinegar, garlic and pepper.',
-		image: 'seafood/FriedBangus.jpg',
-		price : '13.99'
-		
-	},{
-		name : 'Fried Tilapia',
-		description : 'Fried tilapia seasoned with salt and pepper.',
-		image: 'seafood/FriedTilapia.jpg',
-		price : '11.99'
-		
-	},{
-		name : 'Seafood Bicol Express',
-		description : 'Spicy dish composed of shrimp, mussels, squid, cooked in coconut milk with garlic,onion,ginger and chili pepper.',
-		image: 'seafood/SeafoodBicolExpress.jpg',
-		price : '14.99'
-		
-	},{
-		name : 'Seafood Kare-kare (Seafood in Peanut Stew)',
-		description : 'Shrimp, squid and mussels are pan fried and then cooked in a sauce made of peanut and coconut cream with vegetables.',
-		image: 'seafood/SeafoodKareKare.jpg',
-		price : '14.99'
-		
-	},{
-		name : 'Sweet and Sour Fillet',
-		description : 'Deep-fried fish topped with homemade sweeat and sour sauce.',
-		image: 'seafood/SweetAndSourFillet.jpg',
-		price : '11.99'
-		
-	},{
-		name : 'Sisig Bangus (Flaked Milkfish)',
-		description : 'Flaked milkfish with chopped onions and chili peppers bathed with fresh lemon juice and soy sauce.',
-		image: 'seafood/SisigBangus.jpg',
-		price : '13.99'
-		
-	},{
-		name : 'Paksiw na Bangus (Milkfish stewed in Vinegar)',
-		description : 'Milkfish stewed in vinegar simmered in vinegar, garlic, onions and garlic in a delicious broth.',
-		image: 'seafood/PaksiwNaBangus.jpg',
-		price : '14.99'
-		
-	},{
-		name : 'Seafood Kare-kare (Seafood in Peanut Stew)',
-		description : 'Shrimp, squid and mussels are pan fried and then cooked in a sauce made of peanut and coconut cream with vegetables.',
-		image: 'seafood/SeafoodKareKare.jpg',
-		price : '14.99'
-		
-	},{
-		name : 'Sweet and Sour Pampano',
-		description : 'Deep-fried Pampano fish topped with homemade sweet and sour sauce.',
-		image: 'seafood/SweetAndSourPampano.jpg',
-		price : '14.99'
+		name : 'Sinampalukang Manok (Chicken Stewed in Tamarind Soup)',
+		description : 'Chicken cooked ala-Sinigang but uses young tamarind leaves along with the tamarind pulp to sour the broth.',
+		image: 'soup/SinampalukangManok.jpg',
+		price : '12.99'
 		
 	}
 ];
