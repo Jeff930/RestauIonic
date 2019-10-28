06f56ad3a7c180d09ed53ff3eca4428219803d61
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-10-28 23:05:10 +0800

    created noodle model

---
+++ src/models/noodle.ts
@@ -0,0 +1,45 @@
+export const noodles = [
+	{
+		name : 'Bulalo (Beef Marrow Stew)',
+		description : 'Tender beef shanks and ribs in a flavorful noodle with green onions, peppercorn and potatoes.',
+		image: 'noodle/Bulalo.jpg',
+		price : '15.99'
+		
+	},{
+		name : 'Beef Sinigang (Tamarind noodle)',
+		description : 'Beef stewed in a flavorful, slightly sour tamarind broth with lots of vegetables.',
+		image: 'noodle/BeefSinigang.jpg',
+		price : '14.99'
+		
+	},{
+		name : 'Pork Sinigang (Pork Tamarind noodle)',
+		description : 'Pork stewed in a flavorful, slightly sour tamarind broth with lots of vegetables.',
+		image: 'noodle/PorkSinigang.jpg',
+		price : '12.99'
+		
+	},{
+		name : 'Shrimp Sinigang (Shrimp Tamarind noodle)',
+		description : 'Shrimp stewed in a flavorful, slightly sour tamarind broth with lots of vegetables.',
+		image: 'noodle/ShrimpSinigang.jpg',
+		price : '12.99'
+		
+	},{
+		name : 'Bangus Sinigang (Milkfish Tamarind noodle)',
+		description : 'Milkfish stewed in a flavorful, slightly sour tamarind broth with lots of vegetables.',
+		image: 'noodle/MilkfishSinigang.jpg',
+		price : '14.99'
+		
+	},{
+		name : 'Tinolang Manok (Chicken in Ginger noodle)',
+		description : 'Chicken noodle with pepper leaves in broth flavored with ginger, onions and fish sauce.',
+		image: 'noodle/TinolangManok.jpg',
+		price : '12.99'
+		
+	},{
+		name : 'Sinampalukang Manok (Chicken Stewed in Tamarind noodle)',
+		description : 'Chicken cooked ala-Sinigang but uses young tamarind leaves along with the tamarind pulp to sour the broth.',
+		image: 'noodle/SinampalukangManok.jpg',
+		price : '12.99'
+		
+	}
+];
