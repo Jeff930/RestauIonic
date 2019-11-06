68aaa91a7bfde09c739d4e595c601de7e9f6ce65
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-11-06 16:00:24 +0800

    Removed the commented images from models

--- src/models/pork.ts
+++ src/models/pork.ts
@@ -12,33 +12,33 @@ export const porks = [
 		price : '12.99'
 		
 	}
-	// ,{
-	// 	name : 'Lechon Kawali (Crispy Pork Belly)',
-	// 	description : 'Pork belly deep-fried until crisp and golden brown, yet soft and tender inside.',
-	// 	image: 'pork/LechonKawali.jpg',
-	// 	price : '12.99'
+	,{
+		name : 'Lechon Kawali (Crispy Pork Belly)',
+		description : 'Pork belly deep-fried until crisp and golden brown, yet soft and tender inside.',
+		image: 'pork/LechonKawali.jpg',
+		price : '12.99'
 		
-	// }
+	}
 	,{
 		name : 'Inihaw na Liempo (Grilled Pork Belly)',
 		description : 'Grilled pork seasoned with onions,pepper, vinegar and mayonnaise.',
-		image: 'pork/Dinakdakan.jpg',
+		image: 'pork/InihawNaLiempo.jpg',
 		price : '12.99'
 		
 	},{
 		name : 'Dinuguan (Pork Blood Stew)',
 		description : 'Filipino savory pork stew simmered in a rich, spicy dark gravy of pig blood, garlic, chili and vinegar.',
-		image: 'pork/Dinakdakan.jpg',
+		image: 'pork/Dinuguan.jpg',
 		price : '13.99'
 		
 	}
-	// ,{
-	// 	name : 'Menudo',
-	// 	description : 'A tasty boneless pork stew with carrots, potatoes and green peas.',
-	// 	image: 'pork/Menudo.jpg',
-	// 	price : '13.99'
+	,{
+		name : 'Menudo',
+		description : 'A tasty boneless pork stew with carrots, potatoes and green peas.',
+		image: 'pork/Menudo.jpg',
+		price : '13.99'
 		
-	// }
+	}
 	,{
 		name : 'Pork Adobo',
 		description : 'Marinated pork chunks slowly cooked in vinegar and soy sauce with garlic,bay leaf and peppercorn.',
@@ -52,19 +52,19 @@ export const porks = [
 		price : '10.99/2.75'
 		
 	},
-	// {
-	// 	name : 'Sizzling Sisig (Sizzling pork cues)',
-	// 	description : 'Marinated pork cheeks broiled and cut into small pieces served in a sizzling platter with onion and lemon.',
-	// 	image: 'pork/SizzlingSisig.jpg',
-	// 	price : '11.99'
+	{
+		name : 'Sizzling Sisig (Sizzling Pork Cubes)',
+		description : 'Marinated pork cheeks broiled and cut into small pieces served in a sizzling platter with onion and lemon.',
+		image: 'pork/SizzlingSisig.jpg',
+		price : '11.99'
 		
-	// },{
-	// 	name : 'Crispy Sisig (Crispy pork cues)',
-	// 	description : 'Grilled pork cubes with lemon, onions, chillies, scallions and seasoning.',
-	// 	image: 'pork/CrispySisig.jpg',
-	// 	price : '11.99'
+	},{
+		name : 'Crispy Sisig (Crispy Pork Cubes)',
+		description : 'Grilled pork cubes with lemon, onions, chillies, scallions and seasoning.',
+		image: 'pork/CrispySisig.jpg',
+		price : '11.99'
 		
-	// },
+	},
 	{
 		name : 'Crispy Binagoongan (Pork in Shrimp Paste and Coconut Milk)',
 		description : 'Combination of filipino favorites, crispy pork pieces,coconut milk and shrimp paste, toppped with mango salad.',
--- src/models/seafood.ts
+++ src/models/seafood.ts
@@ -1,42 +1,42 @@
 export const seafoods = [
-	// {
-	// 	name : 'Inihaw na Tahong (Grilled Mussels)',
-	// 	description : 'Marinated and grilled mussels.',
-	// 	image: 'seafood/InihawnaTahong.jpg',
-	// 	price : '12.99'
+	{
+		name : 'Inihaw na Tahong (Grilled Mussels)',
+		description : 'Marinated and grilled mussels.',
+		image: 'seafood/InihawNaTahong.jpg',
+		price : '12.99'
 		
-	// }
-	// ,{
-	// 	name : 'Inihaw na Pusit (Grilled Squid)',
-	// 	description : 'Marinated and grilled mussels.',
-	// 	image: 'seafood/InihawnaTahong.jpg',
-	// 	price : '12.99'
+	}
+	,{
+		name : 'Inihaw na Pusit (Grilled Squid)',
+		description : 'Marinated and grilled mussels.',
+		image: 'seafood/InihawNaPusit.jpg',
+		price : '12.99'
 		
-	// },{
-	// 	name : 'Pinaputok na Bangus (Fried and Popped Milkfish)',
-	// 	description : 'Fried/popped Milkfish belly with tomatoes, onions and garlic.',
-	// 	image: 'seafood/PinaputoknaBangus.jpg',
-	// 	price : '14.99'
+	},{
+		name : 'Pinaputok na Bangus (Fried and Popped Milkfish)',
+		description : 'Fried/popped Milkfish belly with tomatoes, onions and garlic.',
+		image: 'seafood/PinaputokNaBangus.jpg',
+		price : '14.99'
 		
-	// },{
-	// 	name : 'Pinaputok na Pampano (Fried and Popped Pampano)',
-	// 	description : 'Fried/popped Pampano fish with tomatoes, onions and garlic.',
-	// 	image: 'seafood/PinaputoknaPampano.jpg',
-	// 	price : '14.99'
+	},{
+		name : 'Pinaputok na Pampano (Fried and Popped Pampano)',
+		description : 'Fried/popped Pampano fish with tomatoes, onions and garlic.',
+		image: 'seafood/PinaputokNaPampano.jpg',
+		price : '14.99'
 		
-	// },{
-	// 	name : 'Fried Bangus (Fried Milkfish)',
-	// 	description : 'Fried marinated milkfish in vinegar, garlic and pepper.',
-	// 	image: 'seafood/FriedBangus.jpg',
-	// 	price : '13.99'
+	},{
+		name : 'Fried Bangus (Fried Milkfish)',
+		description : 'Fried marinated milkfish in vinegar, garlic and pepper.',
+		image: 'seafood/FriedBangus.jpg',
+		price : '13.99'
 		
-	// },{
-	// 	name : 'Fried Tilapia',
-	// 	description : 'Fried tilapia seasoned with salt and pepper.',
-	// 	image: 'seafood/FriedTilapia.jpg',
-	// 	price : '11.99'
+	},{
+		name : 'Fried Tilapia',
+		description : 'Fried tilapia seasoned with salt and pepper.',
+		image: 'seafood/FriedTilapia.jpg',
+		price : '11.99'
 		
-	// },
+	},
 	{
 		name : 'Seafood Bicol Express',
 		description : 'Spicy dish composed of shrimp, mussels, squid, cooked in coconut milk with garlic,onion,ginger and chili pepper.',
@@ -67,13 +67,7 @@ export const seafoods = [
 		image: 'seafood/PaksiwNaBangus.jpg',
 		price : '14.99'
 		
-	},{
-		name : 'Seafood Kare-kare (Seafood in Peanut Stew)',
-		description : 'Shrimp, squid and mussels are pan fried and then cooked in a sauce made of peanut and coconut cream with vegetables.',
-		image: 'seafood/SeafoodKareKare.jpg',
-		price : '14.99'
-		
-	},{
+},{
 		name : 'Sweet and Sour Pampano',
 		description : 'Deep-fried Pampano fish topped with homemade sweet and sour sauce.',
 		image: 'seafood/SweetAndSourPampano.jpg',
