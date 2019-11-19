7f92fcca713961cbcbe00cd263171c0caad48d75
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-11-19 10:29:36 +0800

    Added Contact functionality

---
+++ src/pages/contact/contact.ts
@@ -0,0 +1,70 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ProviderImagesProvider } from '../../providers/provider-images/provider-images';
+
+declare var google;
+/**
+ * Generated class for the ContactPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'contact.html',
+})
+export class ContactPage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  checkApp;
+  date = new Date();
+  timeSched;
+  constructor(public navCtrl: NavController, public imageProvider: ProviderImagesProvider) {
+    this.checkApp = this.imageProvider.checkAppPlatform();
+  }
+
+  ionViewDidLoad() {
+    this.loadMap();
+    if (this.date.getDate() !== 0 ) {
+      this.timeSched = '10:00am - 10:00pm';
+    } else {
+      this.timeSched = '10:00am - 08:00pm';
+    }
+  }
+  callNow() {
+    window.open(`tel:3232290228`, '_system');
+  }
+  loadMap() {
+
+    let latLng = new google.maps.LatLng(34.122198, -118.225819);
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    let marker = new google.maps.Marker({
+      position: latLng,
+      map: mapOptions,
+      title: 'Hello World!'
+    });
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.addMarker(this.map);
+  }
+  addMarker(map: any) {
+
+    let marker = new google.maps.Marker({
+      map: map,
+      animation: google.maps.Animation.DROP,
+      position: map.getCenter()
+    });
+
+    let content = "<h4>Information!</h4>";
+
+    // this.addInfoWindow(marker, content);
+
+
+  }
+
+}
